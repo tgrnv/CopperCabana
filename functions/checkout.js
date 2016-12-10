693dc8ae081ae701f3eb7b6aa425fcd979da04dd
bendobos <bendobos1@gmail.com>
2016-12-11 00:31:43 +0100

    Fixed a bug with entering a credit card number

--- functions/checkout.js
+++ functions/checkout.js
@@ -5,8 +5,12 @@ const data = require('../data.json');
 module.exports = (horseman) => {
   return new Promise((resolve, reject) => {
     horseman
-      .click('.checkout')
+      .open('https://www.supremenewyork.com/checkout')
       .waitForNextPage()
+      .evaluate(function(ccNumber) {
+        var ccField = document.getElementById('cnb');
+        ccField.value = ccNumber;
+      }, data.creditCard.number)
       .type('#order_billing_name', data.buyer.fullName)
       .type('#order_email', data.buyer.email)
       .type('#order_tel', data.buyer.phone)
@@ -17,14 +21,14 @@ module.exports = (horseman) => {
       .type('#order_billing_zip', data.buyer.zip)
       .select('#order_billing_country', data.buyer.country)
       .select('#credit_card_type', data.creditCard.type)
-      .type('#cnb', data.creditCard.number)
       .select('#credit_card_month', data.creditCard.expMonth)
       .select('#credit_card_year', data.creditCard.expYear)
       .type('#vval', data.creditCard.cvv)
       .click('.terms > .icheckbox_minimal > .iCheck-helper')
+      .screenshot('./screenshots/5-Form_Filled.png')
       .click('.checkout')
       .waitForNextPage()
-      .screenshot('./screenshots/5-Copped.png')
+      .screenshot('./screenshots/6-Copped.png')
       .then(() => {
         console.log(chalk.bold(chalk.green(chalk.underline('✅  We copped it! Take a deep breath and enjoy not taking an L!\n'))))
         resolve(horseman);
